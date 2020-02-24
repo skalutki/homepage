d341ec986b4bdc61b9a6f97f10fa2389eb62d47f
skalutki <skalutki@gmail.com>
2020-02-24 06:22:50 +0000

    feat: refactoring css

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ console.log(age);
 console.log(name);
 
 console.log(`Nazywam sie ${name} i mam ${age} lat.`);
+
+const test = document.querySelector('.test__paragraph--js');
+
+console.log(test);
+
+console.log(test.innetHTML);
+
+test.innetHTML = ('teks testowy');
+
+console.log(test);
+
+console.log(test.innetHTML);
