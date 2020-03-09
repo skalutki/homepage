6862bc26c7bca7265f93f25698aef423ce9a9fb2
skalutki <skalutki@gmail.com>
2020-03-09 06:14:17 +0000

    feat: css update and navigation list reffactoring

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ console.log(`Nazywam sie ${name} i mam ${age} lat.`);
 const test = document.querySelector('.test__paragraph--js');
 
 test.innerHTML = `Nazywam sie ${name} i mam ${age} lat.`;
+
+
+function welcome(x, y, z) {
+    const x = 'name';
+    x.name = 'Tomek';
+    const y = 'nick';
+    y.nick = 'Andrzej';
+    const z = 'age';
+    z.age = 22;
+    return;
+}
+
+console.log('witam Cię' (welcome())('na mojej stronie'));
